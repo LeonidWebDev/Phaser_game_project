e7e664030574b01dc1c520aa7246e053eb32e004
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-09-14 04:12:30 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// Our game scene
+let scene = new Phaser.Scene("game");
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: scene
+};
+
+// Create the game with our config values
+// this will also inject our canvas element into the HTML source 
+// for us
+let game = new Phaser.Game(config);
+
+game.init = function() {
+
+};
+
+game.preload = function() {
+
+};
+
+game.create = function() {
+
+};
+
+game.update = function() {
+
+};
+
+
+game.end = function() {
+
+};
